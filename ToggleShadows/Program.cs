b6ef9769ffad96412618307a2b96db034e36bee6
Seth Feuer <lXxMangoxXl@gmail.com>
2020-07-27 09:29:32 -0400

    Add project files.

---
+++ ToggleShadows/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.IO;
+using System.Diagnostics;
+using System.Data;
+
+namespace FixGraphicsEngine
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Finds the config paths and sets them as a variable/string.
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SCP Secret Laboratory/registry.txt");
+
+            //Registry.txt check
+            if (File.Exists(fileName))
+            {
+                Console.WriteLine($"The Registry file is there. Continuing.");
+            }
+            else
+            {
+                Console.WriteLine($"The Registry file is not there. Making the file and continuing.");
+                File.Create(fileName).Close();
+            }
+
+            string text = File.ReadAllText(fileName);
+
+            string Contains = "The Shadows line is there. Continuing.";
+            string DoesNotContain = "The Shadows line is not there. Writing line and continuing.";
+
+
+            //Checks to see if the line is actually there in configs.
+            if (text.Contains("07gfxsets_shadows::-%(|::"))
+            {
+                Console.WriteLine(Contains);
+            }
+            else
+            {
+                Console.WriteLine(DoesNotContain);
+                string billybob = Environment.NewLine + "07gfxsets_shadows::-%(|::1";
+                System.IO.File.AppendAllText(fileName, billybob);
+                NotBarf();
+            }
+
+            //Toggles the config.
+            if (text.Contains("07gfxsets_shadows::-%(|::1"))
+            {
+                text = text.Replace("07gfxsets_shadows::-%(|::1", "07gfxsets_shadows::-%(|::0");
+                File.WriteAllText(fileName, text);
+                Barf();
+            }
+            if (text.Contains("07gfxsets_shadows::-%(|::0"))
+            {
+                text = text.Replace("07gfxsets_shadows::-%(|::0", "07gfxsets_shadows::-%(|::1");
+                File.WriteAllText(fileName, text);
+                NotBarf();
+            }
+            File.WriteAllText(fileName, text);
+            Barf();
+        }
+
+        static void Barf()
+        {
+            Console.WriteLine("Done! Set Shadows to false! Hit any key to quit!");
+            Console.ReadKey();
+            Process.GetCurrentProcess().Kill();
+        }
+
+        static void NotBarf()
+        {
+            Console.WriteLine("Done! Set Shadows to true! Hit any key to quit!");
+            Console.ReadKey();
+            Process.GetCurrentProcess().Kill();
+        }
+        static void UhOh()
+        {
+            // Yes I know it has no references, shhhhh
+            Console.WriteLine($"Something went wrong, please let lXxMangoxXl know about this! Press any key to close the program.");
+            Console.ReadKey();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
+//Shoot me a message on Discord lXxMangoxXl#8878 if theres any issues.
